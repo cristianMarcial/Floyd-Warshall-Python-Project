1598fb71a55d26478ac5a63dcf81e620ee798f95
cristianMacial <cristian.marcial@upr.edu>
2024-03-29 16:03:38 -0400

    Fixed.

--- floydWarshallAlgorithm.py
+++ floydWarshallAlgorithm.py
@@ -1,6 +1,13 @@
-#
-def output(matrix, k):
-    output = [[None for y in range(len(matrix))] for x in range(len(matrix))]
-
-
+def output(matrix, l):
+    output = matrix.copy()
+    for k in range(l):
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if output[i][j] and output[i][k] and output[k][j]:
+                    if output[i][j] > output[i][k] + output[k][j]:
+                        output[i][j] = output[i][k] + output[k][j]
+                elif output[i][k] and output[k][j]:
+                    output[i][j] = output[i][k] + output[k][j]
+    for i in range(len(matrix)): 
+        output[i][i] = 0
     return output
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from sys import argv
-import ast, floydWarshallAlgorithm as fw
+from ast import literal_eval
+import floydWarshallAlgorithm as fw
 
 # Example of an valid input: py main.py [[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]] 1
+
 # Expected result: [[0, 3, inf, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, inf, 0]]
 
 #try:
-if(len(argv) > 1):
-    print(fw.output(ast.literal_eval(argv[1].replace('inf', 'None')), ast.literal_eval(argv[2])))
+if len(argv) > 1:
+    print(fw.output(literal_eval(argv[1].replace('inf', 'None')), literal_eval(argv[2])))
 else:
     print("Invalid input; this only works with more than 3 inputs on the command line.")
 # except:
 #     print('An error has ocurred while reading the previous input.')
+    
+
+
+# py main.py [[0,inf,-2,inf],[4,0,3,inf],[inf,inf,0,2],[inf,-1,inf,0]] 4
+
+# python3 floyd.py [[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]] 1-4
+
+# ecept:
+    #[[0, 3, inf, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, inf, 0]]
+    #[[0, 3, 5, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, 7, 0]]
+    #[[0, 3, 5, 6], [7, 0, 2, 3], [5, 8, 0, 1], [2, 5, 7, 0]]
+    #[[0, 3, 5, 6], [5, 0, 2, 3], [3, 6, 0, 1], [2, 5, 7, 0]]
