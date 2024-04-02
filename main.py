fe9b649df6babc5a4bc57bb6ec4f90a259e30b95
cristianMacial <cristian.marcial@upr.edu>
2024-04-02 12:18:59 -0400

    Fixed.

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ import floydWarshallAlgorithm as fw
 
 # Expected result: [[0, 3, inf, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, inf, 0]]
 
-#try:
-if len(argv) > 1:
-    print(fw.output(literal_eval(argv[1].replace('inf', 'None')), literal_eval(argv[2])))
-else:
-    print("Invalid input; this only works with more than 3 inputs on the command line.")
-# except:
-#     print('An error has ocurred while reading the previous input.')
-    
+try:
+    if len(argv) > 1:
+        print(str(fw.output(literal_eval(argv[1].replace('inf', 'None')), literal_eval(argv[2]))).replace('None', 'inf'))
+    else:
+        print("Invalid input; this only works with more than 3 inputs on the command line.")
+except:
+    print('An error has ocurred while reading the previous input.')
 
+# Tests cases:
 
-# py main.py [[0,inf,-2,inf],[4,0,3,inf],[inf,inf,0,2],[inf,-1,inf,0]] 4
-
-# python3 floyd.py [[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]] 1-4 OR
+#1 py main.py [[0,inf,-2,inf],[4,0,3,inf],[inf,inf,0,2],[inf,-1,inf,0]] 4
+#2 python3 floyd.py [[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]] 1-4 OR
 # py main.py [[0,3,inf,7],[8,0,2,inf],[5,inf,0,1],[2,inf,inf,0]] 4
 
-# ecept:
+# results of #2:
     #[[0, 3, inf, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, inf, 0]]
     #[[0, 3, 5, 7], [8, 0, 2, 15], [5, 8, 0, 1], [2, 5, 7, 0]]
     #[[0, 3, 5, 6], [7, 0, 2, 3], [5, 8, 0, 1], [2, 5, 7, 0]]
